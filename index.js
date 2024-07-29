9194175121405799ab3dd0d11f8bddea1f1e1ec6
kenny <kenny909@mat.umk.pl>
2024-07-29 11:51:21 +0200

    Create simple hello world server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const http = require("http");
+
+const PORT = 5173;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+  res.end(
+    'Hello world'
+  );
+});
+
+server.listen(PORT);
+console.log('server started at port: %d', PORT);
